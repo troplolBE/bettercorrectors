123f4e6d28af1133a6544c84b4fd6d973e2b7b2e
tcastron <thomascastronovo@icloud.com>
2020-11-21 22:45:38 +0100

    Added comments to existing functions

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-# This is a sample Python script.
-import requests
+# imports related to authentication and requests
 from oauthlib.oauth2 import BackendApplicationClient
 from requests_oauthlib import OAuth2Session
 
+# general imports
 import sys
 import time
 
-base_url = 'https://api.intra.42.fr'
+base_url = 'https://api.intra.42.fr'  # base url for all requests made to the api
 
 
 def get_token(client_id, client_secret):
+    """Method to get the OAuth2 session for further requests
+
+    :param client_id: the client_id of the backend application
+    :param client_secret: the secret of the backend application
+    :return session
+    """
     client = BackendApplicationClient(client_id=client_id)
     session = OAuth2Session(client=client)
     session.fetch_token(token_url='https://api.intra.42.fr/oauth/token', client_id=client_id,
@@ -18,6 +24,13 @@ def get_token(client_id, client_secret):
 
 
 def get_all_pages(session, url, size):
+    """Get the data on all pages of a specified url with pagesize size
+
+    :param session: the OAuth2Sesion
+    :param url: url where to do the request
+    :param size: size of the page that gets returned
+    :return: info
+    """
     response = session.request('GET', f'{base_url}{url}', params={'page[size]': size})
     info = response.json()
 
